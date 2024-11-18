68afe28e2ac81dc0a6254d394baa7939fa042cf7
Gabriel Melo <gabrielmelodev321@gmail.com>
2024-11-18 16:39:17 -0300

    Seller class

--- src/main/java/com/daojdbc/Main.java
+++ src/main/java/com/daojdbc/Main.java
@@ -1,11 +1,15 @@
 package com.daojdbc;
 
+import java.util.Date;
+
 import com.daojdbc.models.Department;
+import com.daojdbc.models.Seller;
 
 public class Main {
     public static void main(String[] args) {
 
         Department department = new Department(1, "IT");
-        System.out.println(department);
+        Seller seller = new Seller(1, "Joao", "joaovitor@.com", new Date(), 1000.0, department);
+        System.out.println(seller);
     }
 }
