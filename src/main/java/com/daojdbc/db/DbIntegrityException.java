f951ac2fda42abf4627481d077453ad8738a6fd9
Gabriel Melo <gabrielmelodev321@gmail.com>
2024-11-18 14:19:56 -0300

    first commit

---
+++ src/main/java/com/daojdbc/db/DbIntegrityException.java
@@ -0,0 +1,9 @@
+package com.daojdbc.db;
+
+public class DbIntegrityException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DbIntegrityException(String message) {
+        super(message);
+    }
+}
